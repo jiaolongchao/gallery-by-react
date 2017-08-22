06e8ac67f0a666dbbc3c5ece73368012f9335d94
jiaolongchao <316396083@qq.com>
2017-08-22 21:53:53 +0800

    init project structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,32 @@
-require('normalize.css/normalize.css');
+require('normalize/normalize.css');
 require('styles/App.css');
+require('../styles/main.scss');
 
 
 import React from 'react';
 
-//获取图片名相关的信息
-let imagesDatas = require('../data/imageDatas.json');
-/*let yeomanImage = require('../images/yeoman.png');*/
 
-//利用自执行函数，将图片名信息转换成图片URL路径信息
-imagesDatas = (function genImageURL(imagesDatasArr){  //把所有的图片名称转换成图片路径
-  for(var i=0, j = imagesDatasArr.length; i < j; i++){
-    var singleImageData = imagesDatasArr[i];
+// 获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+
+// 利用自执行函数， 将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
-    imagesDatasArr[i] = singleImageData;
+    imageDatasArr[i] = singleImageData;
   }
-  return imagesDatasArr;
-})(imagesDatas)  //只执行一次的可以使用自执行函数
-imagesDatas = genImageURL(imagesDatas);
+  return imageDatasArr;
+})(imageDatas);
+
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>hello</span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+     <section className = 'stage'>
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+     </section>
     );
   }
 }
