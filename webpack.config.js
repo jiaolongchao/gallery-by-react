e619f563c8f3066ea205cceb5258ee4e8d141821
jiaolongchao <316396083@qq.com>
2017-08-24 23:01:12 +0800

    0824

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,67 @@
-'use strict';
-
-const path = require('path');
-const args = require('minimist')(process.argv.slice(2));
-
-// List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test'];
-
-// Set the correct environment
-let env;
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
-}
-process.env.REACT_WEBPACK_ENV = env;
-
-/**
- * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
- * @return {Object} Webpack config
+/*
+ * Webpack development server configuration
+ *
+ * This file is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
  */
-function buildConfig(wantedEnv) {
-  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
-  let validEnv = isValid ? wantedEnv : 'dev';
-  let config = require(path.join(__dirname, 'cfg/' + validEnv));
-  return config;
-}
+'use strict';
+var webpack = require('webpack');
 
-module.exports = buildConfig(env);
+module.exports = {
+
+  output: {
+    filename: 'main.js',
+    publicPath: '/assets/'
+  },
+
+  cache: true,
+  debug: true,
+  devtool: 'sourcemap',
+  entry: [
+      'webpack/hot/only-dev-server',
+      './src/components/GalleryByReactApp.js'
+  ],
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'styles': __dirname + '/src/styles',
+      'mixins': __dirname + '/src/mixins',
+      'components': __dirname + '/src/components/'
+    }
+  },
+  module: {
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    }],
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel-loader'// transpiling compiling
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "firefox 15"]}!sass-loader?outputStyle=expanded'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "firefox 15"]}'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+
+};
+
